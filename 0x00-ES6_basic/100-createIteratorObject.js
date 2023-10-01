c0e96826446760760e7342d9142c9d282a333319
Peter Boakye <petboa123@gmail.com>
2023-10-01 21:49:57 +0000

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,6 +1,14 @@
-export default function* createIteratorObject(report) {
-  for (const department of report.departments) {
-    for (const employee of department.employees) {
-      yield employee;
+export default function createIteratorObject(report) {
+  function* iterateEmployees() {
+    for (const department in report.allEmployees) {
+      const employeesInDepartment = report.allEmployees[department];
+      for (const employee of employeesInDepartment) {
+        yield employee;
+      }
     }
   }
+
+  return {
+    [Symbol.iterator]: iterateEmployees,
+  };
+}
