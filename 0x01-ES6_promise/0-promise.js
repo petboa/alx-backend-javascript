55d5d34768e20b590eaa74c4af3e152addb7113f
Peter Boakye <petboa123@gmail.com>
2023-10-02 19:21:51 +0000

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,3 @@
-sort default function getResponseFromAPI() {
-    return new Promise((resolve) => {
-	resolve();
-    });
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
